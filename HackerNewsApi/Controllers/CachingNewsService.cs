b5616322021502c7b2dd0c88076f7943f88c6369
Rene Diana <rene.diana@netscrapers.com>
2020-11-09 19:42:51 +0100

    Exchanged httpClient for newsClient

--- HackerNewsApi/Controllers/CachingNewsService.cs
+++ HackerNewsApi/Controllers/CachingNewsService.cs
@@ -9,22 +9,22 @@ namespace HackerNewsApi.Controllers
 {
     public class CachingNewsService : INewsService
     {
-        private HttpClient httpClient;
+        private NewsService newsClient;
+
+        public CachingNewsService(NewsService newsClient)
+        {
+            this.newsClient = newsClient;
+        }
+
         private List<string> topstories = new List<string>();
         private Dictionary<string, Story> stories = new Dictionary<string, Story>();
-
-        public CachingNewsService(HttpClient httpClient)
-        {
-            this.httpClient = httpClient;
-        }
 
         public async Task<IEnumerable<string>> GetTopStories()
         {
             if (topstories.Count == 0)
             {
-                var response = await httpClient.GetAsync("https://hacker-news.firebaseio.com/v0/topstories.json");
-                var json = await response.Content.ReadAsStringAsync();
-                topstories.AddRange(JsonConvert.DeserializeObject<IEnumerable<string>>(json));
+                var response = await newsClient.GetTopStories();
+                topstories.AddRange(response);
 
             }
             return topstories;
@@ -34,9 +34,8 @@ namespace HackerNewsApi.Controllers
         {
             if (!stories.ContainsKey(id))
             {
-                var response = await httpClient.GetAsync($"https://hacker-news.firebaseio.com/v0/item/{id}.json");
-                var json = await response.Content.ReadAsStringAsync();
-                stories[id] = JsonConvert.DeserializeObject<Story>(json);
+                var response = await newsClient.GetStory(id);
+                stories[id] = response;
             }
             return stories[id];
         }
