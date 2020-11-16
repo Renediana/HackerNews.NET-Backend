e9017fc7306737dec7d6bca1e86c976a32efb472
Rene Diana <rene.diana@netscrapers.com>
2020-11-16 14:11:12 +0100

    Adding docker

--- HackerNewsApi/Controllers/CachingNewsService.cs
+++ HackerNewsApi/Controllers/CachingNewsService.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace HackerNewsApi.Controllers
 {
--- HackerNewsApi/Controllers/Vote.cs
+++ HackerNewsApi/Controllers/Vote.cs
@@ -1,6 +1,3 @@
-using System;
-using Newtonsoft.Json;
-
 namespace HackerNewsApi.Controllers
 {
     public class Vote
